f7a4a9eff15f4b5d6e1331aaed6f5c2f256502e8
bernaelo <45598228+bernaelo@users.noreply.github.com>
2019-10-22 15:55:46 +0200

    e

--- app/src/main/java/com/example/myapplication/Modele/MyApplication.java
+++ app/src/main/java/com/example/myapplication/Modele/MyApplication.java
@@ -9,13 +9,13 @@ public class MyApplication {
     private Statistique satiete;
     private Statistique hygiene;
     private Humeur humeur;
-    private Calendar calendar;
+    private Calendrier calendrier;
 
     public MyApplication(){
         energie = new Statistique("Energie");
         satiete = new Statistique("Satiété");
         hygiene = new Statistique("Hygiène");
         humeur = new Humeur();
-        calendar = new GregorianCalendar(2018,9,1,7,0,0);
+        calendrier = new Calendrier();
     }
 }
