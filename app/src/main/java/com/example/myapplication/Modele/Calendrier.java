e9b8d28cb909f7589b6b8efbd3b549cca0aa6842
bernaelo <45598228+bernaelo@users.noreply.github.com>
2019-10-22 15:13:59 +0200

    e

--- app/src/main/java/com/example/myapplication/Modele/Calendrier.java
+++ app/src/main/java/com/example/myapplication/Modele/Calendrier.java
@@ -1,7 +1,13 @@
 package com.example.myapplication.Modele;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Calendrier {
-    private int date; //a voir pour changer pour quelque chose de mieux
-    private String nom;
-    private int heure;
+    private Calendar date;
+
+    public Calendrier(String nom){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy MMM dd HH:mm:ss");
+        Calendar calendar = new GregorianCalendar(2018,9,1,7,0,0);
+    }
 }
--- app/src/main/java/com/example/myapplication/Modele/MyApplication.java
+++ app/src/main/java/com/example/myapplication/Modele/MyApplication.java
@@ -1,13 +1,21 @@
 package com.example.myapplication.Modele;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 
 public class MyApplication {
+
     private Statistique energie;
     private Statistique satiete;
     private Statistique hygiene;
+    private Humeur humeur;
+    private Calendar calendar;
 
     public MyApplication(){
         energie = new Statistique("Energie");
         satiete = new Statistique("Satiété");
         hygiene = new Statistique("Hygiène");
+        humeur = new Humeur();
+        calendar = new GregorianCalendar(2018,9,1,7,0,0);
     }
 }
