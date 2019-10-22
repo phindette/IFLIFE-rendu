5d98d8ebfd26e63f871198938f45b8bc58dd636a
rougiern <nat.rougier38@gmail.com>
2019-10-22 15:00:30 +0200

    Getter & setter de BonusMalus, Competences, Partiel + constructeurs + méthodes "agmenterTaux()" et "diminuerTaux()" de Compétences

--- app/src/main/java/com/example/myapplication/Modele/BonusMalus.java
+++ app/src/main/java/com/example/myapplication/Modele/BonusMalus.java
@@ -4,4 +4,34 @@ public class BonusMalus {
     private String description;
     private int temps;
     private int taux;
+
+    BonusMalus(String description, int temps, int taux) {
+        setDescription(description);
+        setTemps(temps);
+        setTaux(taux);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTemps() {
+        return temps;
+    }
+
+    public void setTemps(int temps) {
+        this.temps = temps;
+    }
+
+    public int getTaux() {
+        return taux;
+    }
+
+    public void setTaux(int taux) {
+        this.taux = taux;
+    }
 }
--- app/src/main/java/com/example/myapplication/Modele/Competences.java
+++ app/src/main/java/com/example/myapplication/Modele/Competences.java
@@ -3,4 +3,33 @@ package com.example.myapplication.Modele;
 public class Competences {
     private String nom;
     private int tauxMaitrise;
+
+    Competences(String nom, int tauxMaitrise) {
+        setNom(nom);
+        setTauxMaitrise(tauxMaitrise);
+    }
+
+    public void augmenterTaux(int nb) {
+        setTauxMaitrise(this.tauxMaitrise + nb);
+    }
+
+    public void diminuerTaux(int nb) {
+        setTauxMaitrise(this.tauxMaitrise - nb);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getTauxMaitrise() {
+        return tauxMaitrise;
+    }
+
+    public void setTauxMaitrise(int tauxMaitrise) {
+        this.tauxMaitrise = tauxMaitrise;
+    }
 }
