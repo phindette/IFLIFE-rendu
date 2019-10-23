8978db24b3ce182364276041dd3d7889cfba0688
Jiyem <45484699+Jiyem@users.noreply.github.com>
2019-10-23 12:53:27 +0200

    Ajout du shop et mise en place des nourritures

--- app/src/main/java/com/example/myapplication/Modele/Nourriture.java
+++ app/src/main/java/com/example/myapplication/Modele/Nourriture.java
@@ -3,5 +3,56 @@ package com.example.myapplication.Modele;
 public class Nourriture {
     private String nom;
     private String description;
-    private int montant;
+    private int montantRegen;
+    private String pathImage;
+    private double cout;
+
+
+    public Nourriture(String nom, String description, int montant, double prix) {
+        this.nom = nom;
+        this.description = description;
+        this.montantRegen = montant;
+        this.cout = prix;
+        this.pathImage = "";
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getMontantRegen() {
+        return montantRegen;
+    }
+
+    public String getPathImage() {
+        return pathImage;
+    }
+
+    public double getCout() {
+        return cout;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setMontantRegen(int montant) {
+        this.montantRegen = montant;
+    }
+
+    public void setPathImage(String pathImage) {
+        this.pathImage = pathImage;
+    }
+
+    public void setCout(double prix) {
+        this.cout = prix;
+    }
 }
