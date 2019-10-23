a8a8cffaaa5bf1a0eaddbe6b02bdc8944bc8414b
Jiyem <45484699+Jiyem@users.noreply.github.com>
2019-10-23 12:43:47 +0200

    Ajout du shop et mise en place des nourritures

--- app/src/main/java/com/example/myapplication/ChambreActivity.java
+++ app/src/main/java/com/example/myapplication/ChambreActivity.java
@@ -2,18 +2,28 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.widgets.ConstraintHorizontalLayout;
 
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.example.myapplication.Modele.MyApplication;
+import com.example.myapplication.Modele.Nourriture;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,7 +36,7 @@ public class ChambreActivity extends AppCompatActivity {
     public ProgressBar barreHygiene;
     public ProgressBar barreHumeur;
     public Thread  thread;
-
+    private ArrayList<Nourriture> nourritures;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +53,14 @@ public class ChambreActivity extends AppCompatActivity {
         barreHygiene = (ProgressBar)  findViewById(R.id.hygieneBar);
         barreHumeur = (ProgressBar)  findViewById(R.id.humeurBar);
 
+        //Initialisation de la nourriture (à modif en DAO)
+        nourritures = new ArrayList<>();
+        init_nourriture();
+
         //Ligne de test pour les barres
         //application.getUtilisateur().augmenterSatiete(150);
+
+
 
 
 
@@ -117,5 +133,100 @@ public class ChambreActivity extends AppCompatActivity {
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
 
+    }
+
+    public void cliqueBtnShop(View w){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ChambreActivity.this);
+        alertDialogBuilder.setTitle("Que voulez-vous acheter?");
+
+        LinearLayout layout = new LinearLayout(this);
+        Button boutonLivre = new Button(this);
+
+        boutonLivre.setText("Livres");
+        boutonLivre.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Toast.makeText(ChambreActivity.this,"clic sur le shop Livres",Toast.LENGTH_SHORT).show();
+            }
+        });
+        layout.addView(boutonLivre);
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+
+        Button boutonNourriture = new Button(this);
+
+        boutonNourriture.setText("Nourritures");
+        boutonNourriture.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                afficheShopNourriture();
+            }
+        });
+
+        layout.addView(boutonNourriture);
+        alertDialogBuilder.setView(layout);
+        AlertDialog alertD = alertDialogBuilder.create();
+        alertD.show();
+
+
+    }
+
+    private void afficheShopNourriture(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ChambreActivity.this);
+        alertDialogBuilder.setTitle("Choisir l'article à acheter");
+
+        ScrollView scrollView = new ScrollView(this);
+        LinearLayout layoutGlobal = new LinearLayout(this);
+        layoutGlobal.setOrientation(LinearLayout.VERTICAL);
+
+        for(int i = 0; i< nourritures.size();i++){
+            LinearLayout layout = new LinearLayout(this);
+            layout.setOrientation(LinearLayout.HORIZONTAL);
+
+            //ajout de l'image
+            File imgFile = new File(nourritures.get(i).getPathImage());
+            if(imgFile.exists()){
+
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+                ImageView myImage = new ImageView(this);
+
+                myImage.setImageBitmap(myBitmap);
+
+                layout.addView(myImage);
+            }
+            //ajout de la regen
+            TextView regen = new TextView(this);
+            regen.setText("restore "+nourritures.get(i).getMontantRegen() + "points de nourriture");
+            layout.addView(regen);
+
+            //ajout du prix
+            TextView prix = new TextView(this);
+            prix.setText("coûte "+nourritures.get(i).getCout()+" € /u");
+            layout.addView(prix);
+            layout.setBackgroundResource(R.drawable.border);
+            layout.setClickable(true);
+
+            layoutGlobal.addView(layout);
+        }
+
+        scrollView.addView(layoutGlobal);
+        alertDialogBuilder.setView(scrollView);
+        AlertDialog alertD = alertDialogBuilder.create();
+        alertD.show();
+    }
+
+
+
+    private void init_nourriture(){
+        Nourriture tacosS = new Nourriture("Tacos simple","Sans tomates, sans salade et c'est parti !",60, 5.0);
+        nourritures.add(tacosS);
+        Nourriture kebab = new Nourriture("Kebab","La viande provient de nos campagnes, le pain de nos boulangers.",50, 5.0);
+        nourritures.add(kebab);
+        Nourriture tacosD = new Nourriture("Tacos double","Sans tomates, sans salade et double viande !",80, 6.5);
+        nourritures.add(tacosD);
+        Nourriture bkebab = new Nourriture("Big Kebab","La viande provient de nos campagnes, le pain de nos boulangers mais il est plus gros! ",60, 5.5);
+        nourritures.add(bkebab);
+        Nourriture patesausel = new Nourriture("Pates au sel","Efficace et pas cher, c'est les pates au sel que je préfère",30, 2.0);
+        nourritures.add(patesausel);
     }
 }
